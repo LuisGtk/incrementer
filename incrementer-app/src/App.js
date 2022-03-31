aabef441e0f3d33858f484a00d7afb5d52c7a901
LuisGtk <luisgonzalez189@gmail.com>
2022-03-31 17:23:41 -0400

    added timer amd buttons

--- incrementer-app/src/App.js
+++ incrementer-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [count, setCount] = useState(0)
+  return <> 
+    Count: {count}
+    <button onClick={() => setCount(0)}>Reset</button>
+    <button onClick={() => setCount(prevCount => prevCount - 1)}>
+      Decrement
+    </button>
+    <button onClick={() => setCount(prevCount => prevCount + 1)}>
+      Increment
+    </button>
+    </>
 }
 
 export default App;
