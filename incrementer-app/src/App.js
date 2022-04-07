34526aae65d5a1008efe40ec2678a8b6dbee654e
LuisGtk <luisgonzalez189@gmail.com>
2022-04-07 16:01:46 -0400

    adding to the css

--- incrementer-app/src/App.js
+++ incrementer-app/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 const App = () => {
+  // 
   const [count, setCount] = useState(0)
-  return <> 
+  return <>
+  <header> 
     Count: {count}
+    </header>
+    <div class="flex"> 
     <button onClick={() => setCount(0)}>Reset</button>
     <button onClick={() => setCount(prevCount => prevCount - 1)}>
       Decrement
@@ -12,7 +16,9 @@ const App = () => {
     <button onClick={() => setCount(prevCount => prevCount + 1)}>
       Increment
     </button>
-    </>
+    </div>
+  </>
+  
 }
 
 export default App;
